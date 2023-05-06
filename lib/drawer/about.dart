4c187b634361cd43b0da212b81967fdebb136fc9
YahyaIshaq <yahya@zaktech.ng>
2023-05-06 14:59:34 +0100

    about

--- lib/drawer/about.dart
+++ lib/drawer/about.dart
@@ -10,7 +10,28 @@ class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Card(
+              elevation: 10,
+              shadowColor: Colors.blue.withOpacity(1),
+              child: Container(
+                height: 400,
+                width: 300,
+                child: Center(
+                  child: Text(
+                    'Card 1',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          
+        ],
+      ),
     );
   }
 }
--- lib/drawer/dashboard.dart
+++ lib/drawer/dashboard.dart
@@ -57,6 +57,7 @@ class _DashboardPageState extends State<DashboardPage> {
                           width: cardWidth,
                           height: 160,
                           child: Card(
+                            shadowColor: Colors.blue.withOpacity(1),
                             elevation: 30,
                             color: Colors.white70,
                             child: Padding(
@@ -126,57 +127,24 @@ class _DashboardPageState extends State<DashboardPage> {
                           height: 160,
                           child: Card(
                             elevation: 30,
+                            shadowColor: Colors.blue.withOpacity(1),
                             color: Colors.white70,
                             child: Padding(
                               padding: const EdgeInsets.only(top: 0),
                               child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Container(
-                                    // width: 70.0,
-                                    // height: 50.0,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(35.0),
-                                      // color: Colors.white,
-                                    ),
-                                    child: Center(
-                                      child: Icon(
-                                        Icons.schedule,
-                                        color: Colors.blue,
-                                        size: 50.0,
-                                      ),
+                                  Text(
+                                    _cards[index * 2 + 1]['header']!,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18.0,
                                     ),
                                   ),
-                                  SizedBox(width: 10.0,
-                                  height: 15,),
-                                  Expanded(
-                                    child: Container(
-                                      height: 120,
-                                      width: MediaQuery.of(context).size.width,
-                                      padding: EdgeInsets.all(10.0),
-                                      color: Colors.blue,
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            _cards[index * 2 + 1]['header']!,
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 18.0,
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                          SizedBox(height: 10.0),
-                                          Text(
-                                            _cards[index * 2 + 1]['text']!,
-                                            style: TextStyle(
-                                              fontSize: 16.0,
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
+                                  SizedBox(height: 10.0),
+                                  Text(
+                                    _cards[index * 2 + 1]['text']!,
+                                    style: TextStyle(
+                                      fontSize: 16.0,
                                     ),
                                   ),
                                 ],
@@ -222,6 +190,7 @@ final List<Map<String, dynamic>> _cards = [
   {
     'header': 'Lectures',
     'text': '#1',
+    'icon': Icons.library_books,
   },
   {
     'header': 'Assignments',
@@ -230,14 +199,17 @@ final List<Map<String, dynamic>> _cards = [
   {
     'header': 'Notes',
     'text': '#3',
+    'icon': Icons.note,
   },
   {
     'header': 'Tests',
     'text': '#4',
+    'icon': Icons.assignment_turned_in,
   },
   {
     'header': 'Exams',
     'text': '#5',
+    'icon': Icons.school,
   },
   {
     'header': 'Total',
